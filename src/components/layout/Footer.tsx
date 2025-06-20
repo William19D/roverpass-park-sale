816cdccbfcbeb903b34bbdcba43b18266b5692f8
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-06-19 23:25:57 -0500

    working footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className="container mx-auto footer">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 py-4">
           <div>
-            <Link to="/">
+            <a href="https://www.roverpass.com/">
               <img
                 alt="RoverPass"
                 title="RoverPass"
@@ -14,21 +14,21 @@ export const Footer = () => {
                 className="mb-3"
                 src="https://d21q6se01pvc3d.cloudfront.net/assets/logos/roverpass-logo-salmon-7131110d6be5bb7723a85b74de54ab9cbf7751df296d28d782340a42e394ad8b.svg"
               />
-            </Link>
+            </a>
             <ul className="list-none p-0 m-0 space-y-1">
-              <li><Link className="text-white hover:text-gray-300" to="/p/terms">Terms</Link></li>
-              <li><Link className="text-white hover:text-gray-300" to="/p/terms/privacy">Privacy Policy</Link></li>
-              <li><Link className="text-white hover:text-gray-300" to="/sitemap">Sitemap</Link></li>
+              <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/p/terms">Terms</a></li>
+              <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/p/terms/privacy">Privacy Policy</a></li>
+              <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/sitemap/">Sitemap</a></li>
             </ul>
             <hr className="border-white mt-2 opacity-50" />
             <div className="mb-3 mt-2">
-              <a className="text-white pr-3 hover:text-gray-300" target="_blank" title="Facebook" href="https://www.facebook.com/RoverPass">
+              <a className="text-white pr-3 hover:text-gray-300" target="_blank" rel="noopener noreferrer" title="Facebook" href="https://www.facebook.com/RoverPass">
                 <i className="fa-brands fa-facebook-f fa-2x" aria-hidden="true"></i>
               </a>
-              <a className="text-white pr-3 hover:text-gray-300" target="_blank" title="Twitter" href="https://twitter.com/Rover_Pass">
+              <a className="text-white pr-3 hover:text-gray-300" target="_blank" rel="noopener noreferrer" title="Twitter" href="https://twitter.com/Rover_Pass">
                 <i className="fa-brands fa-twitter fa-2x" aria-hidden="true"></i>
               </a>
-              <a className="text-white hover:text-gray-300" target="_blank" title="Instagram" href="https://www.instagram.com/roverpass/">
+              <a className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer" title="Instagram" href="https://www.instagram.com/roverpass/">
                 <i className="fa-brands fa-instagram fa-2x" aria-hidden="true"></i>
               </a>
             </div>
@@ -38,10 +38,10 @@ export const Footer = () => {
           <div>
             <strong className="text-lg">Travelers</strong>
             <ul className="list-none p-0 m-0 space-y-1">
-              <li><Link className="text-white hover:text-gray-300" to="/camper-signup">Create Account</Link></li>
-              <li><Link className="text-white hover:text-gray-300" to="/camper-signin">Sign In</Link></li>
-              <li><a className="text-white hover:text-gray-300" target="_blank" href="https://outdoorsyinc.go2cloud.org/aff_c?offer_id=28&aff_id=1258&aff_sub=banner">Roamly RV Insurance</a></li>
-              <li><a className="text-white hover:text-gray-300" target="_blank" href="https://www.roverpass.com/p/trip-essentials">Trip Essentials</a></li>
+              <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/camper-signup/">Create Account</a></li>
+              <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/camper-signin/">Sign In</a></li>
+              <li><a className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer" href="https://outdoorsyinc.go2cloud.org/aff_c?offer_id=28&aff_id=1258&aff_sub=banner">Roamly RV Insurance</a></li>
+              <li><a className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer" href="https://www.roverpass.com/p/trip-essentials">Trip Essentials</a></li>
             </ul>
             <br />
           </div>
@@ -57,7 +57,7 @@ export const Footer = () => {
           <div>
             <strong className="text-lg">Campgrounds</strong>
             <ul className="list-none p-0 m-0 space-y-1">
-              <li><Link className="text-white hover:text-gray-300" to="/owner/">Owner Sign In</Link></li>
+              <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/owner/">Owner Sign In</a></li>
               <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/p/marketplace">Claim Your Campground</a></li>
               <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/p/campground-reservation-software">Campground Reservation Software</a></li>
             </ul>
@@ -67,11 +67,11 @@ export const Footer = () => {
           <div>
             <strong className="text-lg">Company</strong>
             <ul className="list-none p-0 m-0 space-y-1">
-              <li><Link className="text-white hover:text-gray-300" to="/about">About</Link></li>
+              <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/about/">About</a></li>
               <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/blog/">Blog</a></li>
               <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/blog/roverpass-press-releases/">Press</a></li>
               <li><a className="text-white hover:text-gray-300" href="https://help.roverpass.com/">FAQ</a></li>
-              <li><a className="text-white hover:text-gray-300" target="_blank" href="https://help.roverpass.com/support">Contact Us</a></li>
+              <li><a className="text-white hover:text-gray-300" target="_blank" rel="noopener noreferrer" href="https://help.roverpass.com/support">Contact Us</a></li>
               <li><a className="text-white hover:text-gray-300" href="https://www.roverpass.com/p/work-with-us">Advertise With Us</a></li>
             </ul>
             <br />
@@ -79,7 +79,7 @@ export const Footer = () => {
         </div>
 
         <p className="text-center mb-0">
-          © 2025 Roverpass
+          © {new Date().getFullYear()} Roverpass
         </p>
       </div>
     </footer>
